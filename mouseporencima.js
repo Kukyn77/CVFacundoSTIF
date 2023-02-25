c8520697b07f52baa6388554449168965b0b1a3b
Kukyn77 <53490621+Kukyn77@users.noreply.github.com>
2023-02-24 21:45:40 -0300

    Add files via upload

---
+++ mouseporencima.js
@@ -0,0 +1,153 @@
+
+window.onload=function()
+		{
+			var elemento=document.getElementById("Info Instituto");
+			elemento.onmouseover = function(e) {
+ 
+				// El contenido de esta funcion se ejecutara cuanso el mouse
+				// pase por encima del elemento
+ 
+				document.getElementById("Info Instituto").innerHTML="Instituto de Inglés Skills 1996-2001";
+			};
+			elemento.onmouseout = function(e) {
+ 
+				// El contenido de esta funcion se ejecutara cuanso el mouse
+				// salga del elemento
+ 
+				document.getElementById("Info Instituto").innerHTML="Inglés: Nivel Medio";
+			};
+
+			var elemento=document.getElementById("Info Creo");
+			elemento.onmouseover = function(e) {
+ 
+				// El contenido de esta funcion se ejecutara cuanso el mouse
+				// pase por encima del elemento
+ 
+				document.getElementById("Info Creo").innerHTML="CREO: Coaching a través de aprendizaje vivencial";
+			};
+			elemento.onmouseout = function(e) {
+ 
+				// El contenido de esta funcion se ejecutara cuanso el mouse
+				// salga del elemento
+ 
+				document.getElementById("Info Creo").innerHTML="Coaching Transformacional";
+			};
+
+            var elemento=document.getElementById("Catering");
+			elemento.onmouseover = function(e) {
+ 
+				// El contenido de esta funcion se ejecutara cuanso el mouse
+				// pase por encima del elemento
+ 
+				document.getElementById("Catering").innerHTML="Mozo, Encargado de carga y descarga en fábrica";
+			};
+			elemento.onmouseout = function(e) {
+ 
+				// El contenido de esta funcion se ejecutara cuanso el mouse
+				// salga del elemento
+ 
+				document.getElementById("Catering").innerHTML="Josué Echenique Catering";
+
+                
+		}            
+        
+        var elemento=document.getElementById("Dirsalud");
+        elemento.onmouseover = function(e) {
+
+            // El contenido de esta funcion se ejecutara cuanso el mouse
+            // pase por encima del elemento
+
+            document.getElementById("Dirsalud").innerHTML="Área de comunicación, Redes, difusión";
+        };
+        elemento.onmouseout = function(e) {
+
+            // El contenido de esta funcion se ejecutara cuanso el mouse
+            // salga del elemento
+
+            document.getElementById("Dirsalud").innerHTML="2014-2016: Dirección de Salud de berisso"; } 
+        
+            var elemento=document.getElementById("ONG");
+			elemento.onmouseover = function(e) {
+ 
+				// El contenido de esta funcion se ejecutara cuanso el mouse
+				// pase por encima del elemento
+ 
+				document.getElementById("ONG").innerHTML="Capacitaciones en digitalización de datos en REDIP";
+			};
+			elemento.onmouseout = function(e) {
+ 
+				// El contenido de esta funcion se ejecutara cuanso el mouse
+				// salga del elemento
+ 
+				document.getElementById("ONG").innerHTML="2017: ONG Cigob-Registro de las personas de la provincia de buenos Aires";
+        
+                var elemento=document.getElementById("ONG");
+                elemento.onmouseover = function(e) {
+     
+                    // El contenido de esta funcion se ejecutara cuanso el mouse
+                    // pase por encima del elemento
+     
+                    document.getElementById("ONG").innerHTML="Capacitaciones en digitalización de datos en REDIP";
+                };
+                elemento.onmouseout = function(e) {
+     
+                    // El contenido de esta funcion se ejecutara cuanso el mouse
+                    // salga del elemento
+     
+                    document.getElementById("ONG").innerHTML="2017: ONG Cigob-Registro de las personas de la provincia de buenos Aires";
+        
+        
+         
+        } }
+        
+        var elemento=document.getElementById("Anselmi");
+        elemento.onmouseover = function(e) {
+
+            // El contenido de esta funcion se ejecutara cuanso el mouse
+            // pase por encima del elemento
+
+            document.getElementById("Anselmi").innerHTML="Vendedor y manejo de redes sociales";
+        };
+        elemento.onmouseout = function(e) {
+
+            // El contenido de esta funcion se ejecutara cuanso el mouse
+            // salga del elemento
+
+            document.getElementById("Anselmi").innerHTML="2018-2019: Anselmi indumentaria";}
+        
+            var elemento=document.getElementById("360");
+            elemento.onmouseover = function(e) {
+    
+                // El contenido de esta funcion se ejecutara cuanso el mouse
+                // pase por encima del elemento
+    
+                document.getElementById("360").innerHTML="Trabajador Freelance en emprendimiento de manejo de redes-marketing digital";
+            };
+            elemento.onmouseout = function(e) {
+    
+                // El contenido de esta funcion se ejecutara cuanso el mouse
+                // salga del elemento
+    
+                document.getElementById("360").innerHTML="2020-Actualidad: 360 Comunicación Digital";} 
+       
+                var elemento=document.getElementById("software");
+                elemento.onmouseover = function(e) {
+        
+                    // El contenido de esta funcion se ejecutara cuanso el mouse
+                    // pase por encima del elemento
+        
+                    document.getElementById("software").innerHTML="Word, planillas de Excel";
+                };
+                elemento.onmouseout = function(e) {
+        
+                    // El contenido de esta funcion se ejecutara cuanso el mouse
+                    // salga del elemento
+        
+                    document.getElementById("software").innerHTML="Manejo de herramientas Office nivel avanzado";} 
+       
+       
+       
+        }
+
+
+    
